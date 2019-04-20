cc8a101e4c8016c9e115862abdc9d49f645eb045
krzysztofSobinski <krzysztof.sobinski@pracuj.pl>
2019-04-21 00:58:56 +0200

    primeNumberLength value validation

--- PrimeNumbersAdder/Program.cs
+++ PrimeNumbersAdder/Program.cs
@@ -14,15 +14,20 @@ namespace PrimeNumbersAdder
                 .SetBasePath(AppContext.BaseDirectory)
                 .AddJsonFile("appsettings.json")
                 .Build().Get<AppConfiguration>();
+            if (config.PrimeNumberLength > Int32.MaxValue.ToString().Length)
+            {
+                Console.WriteLine($"Parameter {nameof(config.PrimeNumberLength)} value is too big. Max value is {Int32.MaxValue.ToString().Length}");
+                Environment.Exit(0);
+            }
 
             var inputTextToScan = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "inputToScan.txt"));
-
             var primeNumbers = PrimeNumbersFinder.FindWithoutDuplicates(inputTextToScan, config.PrimeNumberLength, config.PrimeNumbersMaxCount);
             if (primeNumbers.Count < config.PrimeNumbersMaxCount)
             {
-                Console.WriteLine($"Alert! Found only {primeNumbers.Count} prime numbers in provided string");
+                Console.WriteLine($"Alert! Found only {primeNumbers.Count} prime numbers in provided string.");
             }
             var sumOfPrimes = primeNumbers.Sum(p => (long)p);
+            Console.WriteLine($"Prime numbers: {string.Join(", ", primeNumbers)}");
             Console.WriteLine($"Sum of prime numbers: {sumOfPrimes}");
         }
     }
